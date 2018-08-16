865c38691fe3682dda3d86db41a3673cf870aada
ignacio <ignaciomenchaca666@gmail.com>
2018-08-16 00:01:20 -0500

    ->

---
+++ main/exercise_1.py
@@ -0,0 +1,8 @@
+"""
+->José Ignacio Menchaca Castro
+->215818166
+->Ejercicio 1
+"""
+print("Tu nombre es: " + input('Escribe tu nombre: '))
+print("Tu edad es: " + input('Escribe tu edad: '))
+print("Tu estatura es: " + input('Escribe tu altura: '))
